c26a4a8c608710fd83f0818b51776afa3fbe00c6
Sivaraj Ponnusamy <rajsivasj@gmail.com>
2024-07-10 22:25:37 +0530

    testcommit

--- src/test/java/testcase/Accrual_AutomationConfig.java
+++ src/test/java/testcase/Accrual_AutomationConfig.java
@@ -8,26 +8,24 @@ import pageObjects.AccrualAutomationConfig;
 public class Accrual_AutomationConfig extends Base_test {
 	@Test
 	public void accrualautomationConfig() throws InterruptedException {
-		AccrualAutomationConfig aac1 = new AccrualAutomationConfig(driver);
+		AccrualAutomationConfig aac = new AccrualAutomationConfig(driver);
 		aac.adminSettings();
 		aac.settingS();
 		aac.accrualautomationcard();
 		Thread.sleep(3000);
 		aac.orgLayout();
 		aac.accountingoptions();
-		//Thread.sleep(6000);
+		// Thread.sleep(6000);
 		try {
-		aac.debitcreditDropdown();
-		Thread.sleep(3000);
-		}
-		catch(Exception e) {
-			System.out.println("Type of Exception :"+e);
-			
+			aac.debitcreditDropdown();
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			System.out.println("Type of Exception :" + e);
+
 		}
 		aac.savebutton();
 		Thread.sleep(3000);
-		
-	}
 
+	}
 
 }
